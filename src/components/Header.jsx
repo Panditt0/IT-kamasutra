0a2b645fef9aa1cc47d0b938a5074bc088988779
Panditt0 <89647268076@mail.ru>
2024-07-06 00:06:50 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1719705600&semt=ais_user' />
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Profile = () => {
+    return <nav className='content'>
+    <div>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIg4KngEq__tCjJ27CJdV_hY6PyBrZ3ujEpA&s' width = '100%'/>
+    </div >
+    <div>
+      <a href='#'>Messages </a>
+    </div>
+    <div>
+      <a href='#'>News </a>
+    </div>
+    <div>
+      <a href='#'>Music </a>
+    </div>
+    <div>
+      <a href='#'>Settings </a>
+    </div>
+  </nav>
+}
+
+export default Profile
