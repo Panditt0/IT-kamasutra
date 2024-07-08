323f4d1357874bbd83c0dbd4b57e08bc8a5c6afa
Panditt0 <89647268076@mail.ru>
2024-07-08 20:43:17 +0300

    logo
    Change logo size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Header.css';
+import style from './Header.module.css'
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={style.header}>
         <img src='https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1719705600&semt=ais_user' />
     </header>
 }
